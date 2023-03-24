c4f912292de75d9cbd5b837874aa7c08cf6bc76f
noahpee <junglebook.noah@gmail.com>
2023-03-24 11:46:59 +0000

    updates

---
+++ scripts.js
@@ -0,0 +1,15 @@
+let name = prompt("Your name:", "");
+document.write("Hello ", name);
+
+let myName = 'noah'
+
+console.log(myName)
+
+let myAge = 31
+
+
+const nameAge = 'my name is ' + myName +' and i am ' + myAge
+
+console.log(nameAge)
+
+
