deae7823e5ceb84ca9c834f9b59e5a517090e561
noahpee <junglebook.noah@gmail.com>
2023-03-27 11:20:01 +0100

    update

--- demoscripts.js
+++ demoscripts.js
@@ -1,2 +1,41 @@
-const demo = document.getElementById('headerMessage')
-demo.innerHTML = message
+function dogOption() {
+
+    dogNumber = document.getElementById('dogSelect').value
+
+    console.log(dogNumber)
+
+    for (i = 0; i < dogNumber; i++) {
+
+        console.log("loadsa dogs")
+
+        document.getElementById('dogImages')
+
+    }
+}
+
+let user;
+
+function yourName() {
+  user = prompt("What is your name?");
+  return document.write(user);
+}
+
+function timeOfDay() {
+  let time = prompt("Hi " + user + ", what hour is it? (0 - 23)");
+
+  let message;
+
+  if (time < 12) {
+    message = "Good morning " + user;
+  } else if (time <= 18) {
+    message = "Good afternoon " + user;
+  } else if (time < 24) {
+    message = "Good night " + user;
+  } else {
+     time = prompt("hi " + user + ' please use real time'); 
+  }
+  console.log(message);
+  return message;
+}
+
+
